e1d3f7c90736bd9c588f85daaf7ed1aee292fd2a
DanrleiB <danrlei.beutler@amo.delivery>
2022-07-11 10:25:23 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:convmoedas/home.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+var request =
+    Uri.parse("https://api.hgbrasil.com/finance?format=json&key=6f301fec");
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: const Conversor(),
+        theme: ThemeData(
+          primaryColor: Colors.black,
+        ));
+  }
+}
+
+Future<Map> getData() async {
+  var response = await http.get(request);
+  return json.decode(response.body);
+}
